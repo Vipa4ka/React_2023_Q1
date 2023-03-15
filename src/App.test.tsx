30f761f013c06da71019697e0fec4f997d68db1a
Vipa4ka <vipa4ka@gmail.com>
2023-03-15 16:13:12 +0200

    init: start project

---
+++ src/App.test.tsx
@@ -0,0 +1,32 @@
+import { describe, it } from 'vitest';
+import { render, screen } from '@testing-library/react';
+
+import { App, WrappedApp } from './App';
+import { MemoryRouter } from 'react-router-dom';
+
+describe('App', () => {
+  it('Renders hello word', () => {
+    // ARRANGE;
+    render(<WrappedApp />);
+    // ACT;
+    // EXPECT;
+    expect(
+      screen.getByRole('heading', {
+        level: 1,
+      })
+    ).toHaveTextContent('Hello word!');
+  });
+
+  it('Renders not found if invalid path', () => {
+    render(
+      <MemoryRouter initialEntries={['/this-route-does-not-exist']}>
+        <App />
+      </MemoryRouter>
+    );
+    expect(
+      screen.getByRole('heading', {
+        level: 1,
+      })
+    ).toHaveTextContent('Not Found');
+  });
+});
---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import { HashRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import NotFound from './pages/NotFoundPage';
+
+export function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </div>
+  );
+}
+
+export function WrappedApp() {
+  return (
+    <HashRouter>
+      <App />
+    </HashRouter>
+  );
+}
