cabbc7b479dc418bf9888791739da710010fdf18
Vipa4ka <vipa4ka@gmail.com>
2023-03-26 12:58:17 +0300

    feat: add avatar component

--- src/components/Forms/Forms.tsx
+++ src/components/Forms/Forms.tsx
@@ -1,5 +1,6 @@
 import styles from './Forms.module.css';
 import { Component, createRef } from 'react';
+import avatarImg from './avatar.png';
 
 class Forms extends Component {
   constructor(props: Record<string, never>) {
@@ -10,8 +11,8 @@ class Forms extends Component {
     this.birthdate = createRef();
     this.female = createRef();
     this.male = createRef();
-    // this.surname = createRef();
-    // this.surname = createRef();
+    this.country = createRef();
+    this.avatar = createRef();
     // this.surname = createRef();
   }
   name: React.RefObject<HTMLInputElement>;
@@ -19,8 +20,8 @@ class Forms extends Component {
   birthdate: React.RefObject<HTMLInputElement>;
   female: React.RefObject<HTMLInputElement>;
   male: React.RefObject<HTMLInputElement>;
-  // surname: React.RefObject<HTMLInputElement>;
-  // surname: React.RefObject<HTMLInputElement>;
+  country: React.RefObject<HTMLSelectElement>;
+  avatar: React.RefObject<HTMLInputElement>;
   // surname: React.RefObject<HTMLInputElement>;
   // surname: React.RefObject<HTMLInputElement>;
   // surname: React.RefObject<HTMLInputElement>;
@@ -28,23 +29,43 @@ class Forms extends Component {
   handleChange(event: React.SyntheticEvent) {
     event.preventDefault();
 
-    const { name, surname, birthdate, female, male } = this;
+    const { name, surname, birthdate, female, male, country, avatar } = this;
+
     const currentName = (name.current as HTMLInputElement).value;
     const currentSurname = (surname.current as HTMLInputElement).value;
     const currentBirthdate = (birthdate.current as HTMLInputElement).value;
     const currentFemale = (female.current as HTMLInputElement).value;
     const currentMale = (male.current as HTMLInputElement).value;
+    const currentCountry = (country.current as HTMLSelectElement).value;
+    const currentAvatar = (avatar.current as HTMLInputElement).value;
     // const currentSurname = (surname.current as HTMLInputElement).value;
     // const currentSurname = (surname.current as HTMLInputElement).value;
     // const currentSurname = (surname.current as HTMLInputElement).value;
-    // const currentSurname = (surname.current as HTMLInputElement).value;
-    // const currentSurname = (surname.current as HTMLInputElement).value;
-    console.log(currentName, currentSurname, currentBirthdate, currentFemale, currentMale);
+    console.log(
+      currentName,
+      currentSurname,
+      currentBirthdate,
+      currentFemale,
+      currentMale,
+      currentCountry,
+      currentAvatar
+    );
   }
 
   render() {
     return (
       <form className={styles.list} onSubmit={this.handleChange}>
+        <label>
+          <img className={styles.avatar} src={avatarImg} alt="avatarImg" />
+          <input
+            className={styles.input_avatar}
+            type="file"
+            name="avatar"
+            ref={this.avatar}
+            placeholder="Avarat"
+            accept="image/*"
+          />
+        </label>
         <label>
           <input
             type="text"
@@ -76,19 +97,22 @@ class Forms extends Component {
             required
           />
         </label>
+
+        <label htmlFor="country">Your country:</label>
+        <select className={styles.input} id="country" name="country" ref={this.country} required>
+          <option value="">Choose the country</option>
+          <option value="Poland">Poland</option>
+          <option value="Bulgaria">Bulgaria</option>
+          <option value="Germany">Germany</option>
+          <option value="Romania">Romania</option>
+          <option value="other">other</option>
+        </select>
         <div className={styles.gender}>
-          <label>
+          <label className={styles.gender_radio}>
             Male
-            <input
-              type="radio"
-              name="gender"
-              value="male"
-              ref={this.male}
-              className={styles.gender_radio}
-              required
-            />
+            <input type="radio" name="gender" value="male" ref={this.male} required />
           </label>
-          <label>
+          <label className={styles.gender_radio}>
             Female
             <input
               type="radio"
