3438e8d83b7c247e97cfa3df152df8c7d24ab767
Vipa4ka <vipa4ka@gmail.com>
2023-03-30 18:06:12 +0300

    fix: cardForm in function

--- src/components/Forms/CardForm.tsx
+++ src/components/Forms/CardForm.tsx
@@ -1,36 +1,31 @@
-import { Component } from 'react';
 import styles from './FormsCards.module.css';
 
 import { FormProps } from '../../pages/Form';
 
-class CardForm extends Component<FormProps> {
-  render() {
-    const { cards } = this.props;
-
-    return (
-      <div>
-        <ul className={styles.group}>
-          {cards?.map(
-            ({ id, name, surname, birthdate, gender, country, avatar, consent, consentNews }) => {
-              return (
-                <li key={id} className={styles.list_cards}>
-                  <img src={avatar} alt="avatar" className={styles.avatarFoto} />
-                  <p>
-                    {name} {surname}
-                  </p>
-                  <p>Birthdate: {birthdate}</p>
-                  <p>Gender: {gender}</p>
-                  <p>Country: {country}</p>
-                  <p>Consent to personal data: {consent}</p>
-                  <p>Consent to receive news: {consentNews}</p>
-                </li>
-              );
-            }
-          )}
-        </ul>
-      </div>
-    );
-  }
-}
+const CardForm = ({ cards }: FormProps) => {
+  return (
+    <div>
+      <ul className={styles.group}>
+        {cards?.map(
+          ({ id, name, surname, birthdate, gender, country, avatar, consent, consentNews }) => {
+            return (
+              <li key={id} className={styles.list_cards}>
+                <img src={avatar} alt="avatar" className={styles.avatarFoto} />
+                <p>
+                  {name} {surname}
+                </p>
+                <p>Birthdate: {birthdate}</p>
+                <p>Gender: {gender}</p>
+                <p>Country: {country}</p>
+                <p>Consent to personal data: {consent}</p>
+                <p>Consent to receive news: {consentNews}</p>
+              </li>
+            );
+          }
+        )}
+      </ul>
+    </div>
+  );
+};
 
 export default CardForm;
