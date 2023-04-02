97135944fd508af61654e2c3d6a1c45feefd13ab
Vipa4ka <vipa4ka@gmail.com>
2023-04-02 17:14:59 +0300

    fix: components form

--- src/components/Forms/formsComponent/CheckboxConsent.tsx
+++ src/components/Forms/formsComponent/CheckboxConsent.tsx
@@ -1,18 +1,18 @@
-import styles from '../Forms.module.css';
-import { FormProps } from '../../../pages/Form';
+// import styles from '../Forms.module.css';
+// import { FormProps } from '../../../pages/Form';
 
-export default function CheckboxConsent({ checkboxConsentProp }: FormProps) {
-  return (
-    <label htmlFor="checkboxConsent" className={styles.label_checkbox}>
-      <input
-        className={styles.checkbox}
-        type="checkbox"
-        name="checkboxConsent"
-        id="checkboxConsent"
-        ref={checkboxConsentProp}
-      />
-      I consent to my personal data
-      {/* {checkboxConsentInput && <div className={styles.input_err}>{checkboxConsentInput}</div>} */}
-    </label>
-  );
-}
+// export default function CheckboxConsent({ checkboxConsentProp }: FormProps) {
+//   return (
+//     <label htmlFor="checkboxConsent" className={styles.label_checkbox}>
+//       <input
+//         className={styles.checkbox}
+//         type="checkbox"
+//         name="checkboxConsent"
+//         id="checkboxConsent"
+//         ref={checkboxConsentProp}
+//       />
+//       I consent to my personal data
+//       {/* {checkboxConsentInput && <div className={styles.input_err}>{checkboxConsentInput}</div>} */}
+//     </label>
+//   );
+// }
